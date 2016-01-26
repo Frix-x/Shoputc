9e3b9b955d1752799f5ab1351dfe2525745a00cc
frix_x <boisselier.felix@gmail.com>
2016-01-26 11:01:03 +0000

    Capitalize first letter of name/lastname during signup

--- client/js/login.js
+++ client/js/login.js
@@ -1,16 +1,20 @@
 /* global Router*/
 /* global sAlert */
 
+String.prototype.capitalizeFirstLetter = function() {
+	return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+};
+
 Template.login.events({
-    "click #go_register": function(e, t) {
+	"click #go_register": function(e, t) {
 
 		var email = $('input[name="nu_email"]').val();
 		var password = $('input[name="nu_password"]').val();
 		var profile = {
-            name: $('input[name="nu_name"]').val(),
-            lastname: $('input[name="nu_lastname"]').val(),
-            coloc: '',
-            debt: 0
+			name: $('input[name="nu_name"]').val().capitalizeFirstLetter(),
+			lastname: $('input[name="nu_lastname"]').val().capitalizeFirstLetter(),
+			coloc: '',
+			debt: 0
 		};
 
 		var user = {
@@ -21,8 +25,13 @@ Template.login.events({
 
 		Accounts.createUser(user, function(err) {
 			if (err) {
-				sAlert.error(err.reason, {effect: 'bouncyflip', position: 'top-right', onRouteClose: false});
-			} else {
+				sAlert.error(err.reason, {
+					effect: 'bouncyflip',
+					position: 'top-right',
+					onRouteClose: false
+				});
+			}
+			else {
 				Router.go('home');
 			}
 		});
@@ -30,14 +39,20 @@ Template.login.events({
 });
 
 Template.login.events({
-    "click #go_connect": function(e, t) {
+	"click #go_connect": function(e, t) {
 
 		var user = $("input[name='login_email']").val();
 		var password = $("input[name='login_password']").val();
 
-		Meteor.loginWithPassword({email: user}, password, function(err) {
+		Meteor.loginWithPassword({
+			email: user
+		}, password, function(err) {
 			if (err) {
-				sAlert.error(err.reason, {effect: 'bouncyflip', position: 'top-right', onRouteClose: false});
+				sAlert.error(err.reason, {
+					effect: 'bouncyflip',
+					position: 'top-right',
+					onRouteClose: false
+				});
 			}
 		});
 	}
@@ -45,14 +60,14 @@ Template.login.events({
 
 Template.login.events({
 	"click #inscription": function(e, t) {
-        $('#register').removeClass('hidden');
-        $('#login').addClass('hidden');
+		$('#register').removeClass('hidden');
+		$('#login').addClass('hidden');
 	}
 });
 
 Template.login.events({
 	"click #dejauser": function(e, t) {
-        $('#login').removeClass('hidden');
-        $('#register').addClass('hidden');
+		$('#login').removeClass('hidden');
+		$('#register').addClass('hidden');
 	}
 });
